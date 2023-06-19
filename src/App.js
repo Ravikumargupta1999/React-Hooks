d154ef12fe15c0378278a1ca18a2cd3eed3e0a95
unknown <ravikumargupta391999@gmail.com>
2023-06-19 18:41:23 +0530

    useEffect() Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Input from "./Components/InputFormWithClass";
-
+// import Input from "./Components/InputFormWithClass";
+import Input from "./Components/InputFormWithFunction";
 function App() {
   return (
     <>
--- src/Components/InputFormWithFunction.js
+++ src/Components/InputFormWithFunction.js
@@ -1,26 +1,32 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+
 export default function Input() {
 
     const [name, setName] = useState("");
     const [lastName, setLastName] = useState("");
+
+    useEffect(() =>{
+        document.title = name + "" + lastName;
+    },[lastName])  // empty array will make it act like only componentDidMount
     return (
         <>
             <div className="section">
                 <Row label="Name">
                     <input value={name}
-                    onChange={(e) => setName(e.target.value)} />
+                        onChange={(e) => setName(e.target.value)} />
                 </Row >
                 <Row label="Last Name">
                     <input value={lastName}
-                    onChange={(e) => setLastName(e.target.value)} />
+                        onChange={(e) => setLastName(e.target.value)} />
                 </Row >
             </div>
 
             <h2>Hello,{name + " " + lastName} </h2>
-
         </>
     )
 }
+
+
 
 
 function Row(props) {
